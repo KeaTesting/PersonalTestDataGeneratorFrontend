77f85164dfbddbe9cb23b2bcfbb95a5455ffd0b3
Troels-dat21B <troe1912@dat.stud.kea>
2024-10-14 12:27:01 +0200

    added the cringe "=" to line 11

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.querySelector('#frmGenerate').addEventListener('submit', (e) => {
     if (e.target.chkPerson.checked) {
         endpoint += 'person'
         const numPersons = parseInt(e.target.txtNumberPersons.value);
-        if (numPersons > 1) {
+        if (numPersons >= 1) {
             endpoint += '?n=' + numPersons;
         }
     } else {
